2156b0d3477fd352f3ae93aecb9f08534157d99a
jenaalsup <63277580+jenaalsup@users.noreply.github.com>
2020-06-17 17:20:03 -0700

    Add parallelism part 1

--- segment_size_2.py
+++ segment_size_2.py
@@ -5,94 +5,115 @@ import time
 from price_parser import Price
 from multiprocessing import Pool
 
-# automatically reads in the next unread line so no need for a line number parameter
-def line_from_csv(f):
-  ln = f.readline()
-  if ln == "":
+header = ''
+# indexes of each category
+BRAND = 0
+URL = 1
+SHOPIFY = 2
+SALES = 3
+ALEXA = 4
+EMPLOYEES = 5
+SIZE = 6
+FOUNDER = 7
+CONTACT = 8
+
+
+def pull_brand(data):
+  # do slow things <--
+  # do NOT write to csv here.
+  brand = data[BRAND]
+  url = data[URL]
+  size = data[SIZE]
+  annual_sales = get_annual_sales(url)
+  data[SALES] = annual_sales
+  alexa_rating = get_alexa_rating(url)
+  data[ALEXA] = alexa_rating
+  num_employees = get_num_employees(brand)
+  data[EMPLOYEES] = num_employees
+
+  if size == '': # don't overwrite a size that is already present in the csv
+    if annual_sales != 'URL Failed':
+      annual_sales = float(annual_sales)
+      if annual_sales <= 10000000: # small <= 10m
+        data[SIZE] = 'Small'
+      if annual_sales >= 100000000: # large >= 100m
+        data[SIZE] = 'Large'
+      else: # medium is between small and large
+        data[SIZE] = 'Medium'
+    elif num_employees != 'Brand Failed':
+      num_employees = float(num_employees)
+      if num_employees <= 10: # small <= 10
+        data[SIZE] = 'Small'
+      if num_employees >= 100: # large >= 100
+        data[SIZE] = 'Large'
+      else: # medium is between small and large
+        data[SIZE] = 'Medium'
+    else:
+      data[SIZE] = '?Small' # set to ?Small if unable to parse info on the brand
+
+  return data # return the line needed to write to the csv
+
+# incomplete
+def pull_raw_data(brands, urls, sizes):
+  arguments = [(brands[i], urls[i], sizes[i]) for i in range(len(brands))]
+  with Pool(8) as p:
+      all_pulled_data = p.map(pull_brand, arguments)
+
+  print(f'all pulled data: {all_pulled_data}')
+  return all_pulled_data
+
+# incomplete
+def process_data(database):
+  for 
+  database = pull_raw_data(1, 2, 3)
+
+def read_data(file_path):
+  global header
+  file_name = file_path
+  fi = open(file_name, "r") # r stands for read
+  if fi == None:
+    print("Could not open csv file: ", file_name)
     return None
-  print( "line: ", ln, end = "" )
-  i = 0
-  li = 0
-  parsed_line = []
-  while True:
-    start = i
-    while( i < len(ln) and ln[i] != ',' ):
-      i = i + 1
-    if i < len(ln):
-      parsed_line.insert( li, ln[start:i] )
-    li = li + 1
-    i = i + 1
-    if i >= len(ln):
+  database = []
+  header = fi.readline()
+  for i in range(100000):
+    parsed_line = line_from_csv(fi)
+    if parsed_line == None:
       break
-  return parsed_line
+    database.insert(i, parsed_line)
+  fi.close()
+  return database
 
-# parses url from csv and parsed_line
-def process_file(file_path, start, end):
-    file_name = file_path
-    fi = open(file_name, "r") # r stands for read
-    if fi == None:
-      print("Could not open csv file: ", file_name)
-      return None
-    
-    file_name_result = "result_after_scraping.csv"
-    fo = open(file_name_result, "w+") # r stands for write over
-    if fo == None:
-      print("Could not create csv file: ", file_name_result)
-      return None
+# only write the database to csv don't change the database at all
+def write_data_to_csv(database):
+  file_name_result = "result_after_scraping.csv"
+  fo = open(file_name_result, "w+") # r stands for write over
+  if fo == None:
+    print("Could not create csv file: ", file_name_result)
+    return None
 
-    # copy over the header
-    ln = fi.readline()
-    fo.write(ln)
+  # header
+  fo.write(header)
+  
+  for data in database:
+    str_data = ''
+    for item in data:
+      str_data = str_data + item + ","
+    str_data = str_data[0:len(str_data)-1]
+    fo.write(str_data) 
+    fo.write("\r\n")
+  fo.close()
 
-    while True:
-      parsed_line = line_from_csv(fi)
-      if parsed_line == None: # reached the last line of the csv
-        break
-      if len(parsed_line) < 1:
-        fo.write("\r\n")
-        continue
-      brand = parsed_line[0]
-      url = parsed_line[1]
-      annual_sales = get_annual_sales(url)
-      parsed_line[3] = annual_sales
-      alexa_rating = get_alexa_rating(url)
-      parsed_line[4] = alexa_rating
-      num_employees = get_num_employees(brand)
-      parsed_line[5] = num_employees
+# helper methods
 
-      if parsed_line[6] == '': # don't overwrite a size that is already present in the csv
-        if annual_sales != 'URL Failed':
-          annual_sales = float(annual_sales)
-          if annual_sales <= 10000000: # small <= 10m
-            parsed_line[6] = 'Small'
-          if annual_sales >= 100000000: # large >= 100m
-            parsed_line[6] = 'Large'
-          else: # medium is between small and large
-            parsed_line[6] = 'Medium'
-        elif num_employees != 'Brand Failed':
-          num_employees = float(num_employees)
-          if num_employees <= 10: # small <= 10
-            parsed_line[6] = 'Small'
-          if num_employees >= 100: # large >= 100
-            parsed_line[6] = 'Large'
-          else: # medium is between small and large
-            parsed_line[6] = 'Medium'
-        else:
-          parsed_line[6] = '?Small' # set to ?Small if unable to parse info on the brand
+def get_brands(file_path):
+  pass
 
+def get_urls(file_path):
+  pass
 
-      if parsed_line:
-        line = ""
-        for l in parsed_line:
-          line = line + l + ","
-        line = line[0:len(line)-1]
-        fo.write(line)
-        fo.write("\r\n")
-
-      else:
-        print("No data scraped")
-    fi.close()
-    fo.close()
+def get_sizes(file_path):
+  pass
 
 def get_annual_sales(url): 
   url = 'https://ecommercedb.com/en/store/{0}'.format(url)
@@ -191,28 +212,33 @@ def get_num_employees(brand):
   num_employees = ''.join([c for c in value if c in '1234567890'])
   return str(num_employees)
 
-def save_scraped_data(lines):
-  if lines:
-      file_name = "result_after_scraping.csv"
-      f = open(file_name,"w+")
-      f.write("Brand name", "Brand URL", "Shopify or other","Annual sales", "Alexa rating", "# of employees,Segment (s/m/l/d)", "Name/Founder 1,Contact email""\r\n")
-      for line in lines:
-          f.write(line + "\r\n")
-      f.close() 
-  else:
-      print("No data scraped")
-  return
+def line_from_csv(f):
+  ln = f.readline()
+  if ln == "":
+    return None
+  print( "line: ", ln, end = "" )
+  i = 0
+  li = 0
+  parsed_line = []
+  while True:
+    start = i
+    while( i < len(ln) and ln[i] != ',' ):
+      i = i + 1
+    if i < len(ln):
+      parsed_line.insert( li, ln[start:i] )
+    li = li + 1
+    i = i + 1
+    if i >= len(ln):
+      break
+  return parsed_line
 
 # main code entry point
 if __name__=="__main__":
   argparser = argparse.ArgumentParser()
   argparser.add_argument('path',help = 'Path to CSV')
-  argparser.add_argument('start',help = 'Starting line in CSV') # includes starting line
-  argparser.add_argument('end',help = 'Ending line in CSV') # includes ending line
   args = argparser.parse_args()
   file_path = args.path
-  start = args.start
-  end = args.end
-  print(">> ", file_path)
-  process_file(file_path, start, end)
-  # done
+  database = read_data(file_path)
+  #raw_data = pull_raw_data(brands, urls, sizes)
+  process_data(database)
+  write_data_to_csv(database)
