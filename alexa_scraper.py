927ff6dc850c915bfe4faf333702877b94147437
jenaalsup <63277580+jenaalsup@users.noreply.github.com>
2020-06-18 16:36:19 -0700

    Make alexa scraping separate

--- alexa_scraper.py
+++ alexa_scraper.py
@@ -20,48 +20,10 @@ CONTACT = 8
 
 
 def pull_brand(data):
-  # do slow things <--
-  # do NOT write to csv here.
-  brand = data[BRAND]
   url = data[URL]
-  size = data[SIZE]
-  annual_sales = get_annual_sales(url)
-  data[SALES] = annual_sales
-  num_employees = get_num_employees(brand)
-  data[EMPLOYEES] = num_employees
-
-  if size == '': # don't overwrite a size that is already present in the csv
-    if annual_sales != 'URL Failed':
-      try:
-        annual_sales = float(annual_sales)
-      except ValueError:
-        annual_sales = 0
-      if annual_sales == 0:
-        print("BROKEN")
-        data[SIZE] = '?Small'
-      elif annual_sales <= 10000000: # small <= 10m
-        data[SIZE] = 'Small'
-      elif annual_sales >= 100000000: # large >= 100m
-        data[SIZE] = 'Large'
-      else: # medium is between small and large
-        data[SIZE] = 'Medium'
-    elif num_employees != 'Brand Failed':
-      try:
-        num_employees = float(num_employees)
-      except ValueError:
-        num_employees = 0    
-      if num_employees == 0:
-        print("BROKEN")
-        data[SIZE] = '?Small'
-      elif num_employees <= 10: # small <= 10
-        data[SIZE] = 'Small'
-      elif num_employees >= 100: # large >= 100
-        data[SIZE] = 'Large'
-      else: # medium is between small and large
-        data[SIZE] = 'Medium'
-    else:
-      data[SIZE] = '?Small' # set to ?Small if unable to parse info on the brand
-
+  alexa_rating = get_alexa_rating(url)
+  if data[ALEXA] == '':
+    data[ALEXA] = alexa_rating
   return data # return the line needed to write to the csv
 
 def process_data(database):
@@ -90,7 +52,7 @@ def read_data(file_path):
 
 # only write the database to csv don't change the database at all
 def write_data_to_csv(database):
-  file_name_result = "result_after_scraping.csv"
+  file_name_result = "result_after_scraping_alexa.csv"
   fo = open(file_name_result, "w+") # r stands for write over
   if fo == None:
     print("Could not create csv file: ", file_name_result)
@@ -110,8 +72,8 @@ def write_data_to_csv(database):
 
 # helper methods
 
-def get_annual_sales(url): 
-  url = 'https://ecommercedb.com/en/store/{0}'.format(url)
+def get_alexa_rating(url): 
+  url = 'https://www.alexa.com/siteinfo/{0}'.format(url)
   headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'}
   failed = False
 
@@ -130,51 +92,17 @@ def get_annual_sales(url):
         break
 
   if failed:
-    print("The ecommercedb.com network is unresponsive or url error. Please try again later (or now).")
+    print("The alexa.com network is unresponsive or url error. Please try again later (or now).")
     return "URL Failed"
 
-  raw_value = parser.xpath('//div[contains(@class, "fancyBox__content")]//text()')
-  value = raw_value[0].strip()
-  if value[len(value) - 1] == 'm':
-    magnitude = 1000000
-  else:
-    magnitude = 1
-  number_value = value[3: len(value) - 1]
-  price = Price.fromstring(number_value).amount_float
-  price = price * magnitude
-  price = str(price)
-  return price
-
-def get_num_employees(brand): 
-  url = 'https://www.google.com/search?q={0} number of employees'.format(brand)
-  headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'}
-  failed = False
-
-  # Retries 5 times for handling network errors
-  for _ in range(2):
-    print ("Retrieving %s"%(url)) 
-    response = requests.get(url, headers=headers, verify=True)
-    parser = html.fromstring(response.text)
-    print("Done retrieving - status code: ", response.status_code)
-
-    if response.status_code!=200:
-        failed = True
-        continue
-    else:
-        failed = False
-        break
-
-  if failed:
-    print("The google.com network is unresponsive or url error. Please try again later (or now).")
-    return "Brand Failed"
-
-  raw_value = parser.xpath('//div[contains(@class, "Z0LcW XcVN5d AZCkJd")]//text()')
+  raw_value = parser.xpath('//div[contains(@class, "rankmini-rank")]//text()') # some sites use rankmini-rank
   if len(raw_value) < 1:
-    return "Brand Failed"
-  value = raw_value[0].strip()
-  num_employees = ''.join([c for c in value if c in '1234567890'])
-  return str(num_employees)
-
+    raw_value = parser.xpath('//p[contains(@class, "big data")]//text()') # some sites use big data
+    if len(raw_value) < 1:
+      return "URL Failed"
+  value = raw_value[2].strip()
+  ranking = ''.join([c for c in value if c in '1234567890'])
+  return str(ranking)
 
 def line_from_csv(f):
   ln = f.readline()
